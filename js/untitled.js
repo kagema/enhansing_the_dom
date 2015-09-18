46ec304ef9f04e91442cd9b2cd7cdb6e9dfcca63
Ivan <kagemasan@gmail.com>
2015-09-18 10:47:00 +0300

    realy brooral adn bad commit
    modified:   js/untitled.js
    
    only add appendBeforeLastScript function  and some prepearig stuf

--- js/untitled.js
+++ js/untitled.js
@@ -1,7 +1,7 @@
 (function(){
   'use strict';
   
-  var app = function () {
+  var app = (function () {
     
     var data = {
       overlay: null,
@@ -10,29 +10,53 @@
     };
 
     function init () {
+      data.list = document.querySelector('.img-list');
+
+
       data.overlay = document.createElement('div');
       data.overlay.setAttribute('class', 'overlay');
       data.overlay.width = window.innerWidth + 'px';
       data.overlay.height = window.innerHeight + 'px';
-      document.body.appendChild(data.overlay);
-
+      console.log(data);
     }
 
     function action () {
-      window.addEventListener('resize', function () {
+      document.addEventListener('resize', function () {
         if(data.overlay) {
-          data.overlay.width = window.innerWidth + 'px';
-          data.overlay.height = window.innerHeight + 'px'; 
+          data.overlay.width =  document.documentElement.clientWidth + 'px';
+          data.overlay.height =  document.documentElement.clientHeight + 'px'; 
         }
       }, false);
+
+      data.list.addEventListener('click', function (evt) {
+        if(evt.target.tagName === 'IMG') {
+          
+        }
+      }, false);
+    }
+
+    /**
+     * find last element in and if it's SCRIPT tag
+     * if goes up and if there are stack of SCRIPT elements, it append passed 
+     * element before first SCRIPT element
+     */
+    
+    function appendBeforeLastScript (overlay) {
+      
+      var lastElement = document.body.lastElementChild;
+      while(lastElement.tagName === 'SCRIPT') {
+        lastElement = lastElement.previousSibling;
+      }
+      lastElement = lastElement.nextSibling;
+      document.body.insertBefore(data.overlay, lastElement);
     }
 
 
 
     return {
-      'init' : 'init'
+      'init' : init
     };
-  };
+  }());
 
   app.init();
 }());
